5118efb5b9e0d9190382ffca114f0c45fa86f1e2
Bhaumik Gadani <bhaumikwork@gmail.com>
2018-05-31 13:10:36 +0530

    remove recoberable

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+        :rememberable, :trackable, :validatable
   # Email validation
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
