6ab4bf9f6ea759fb50b0ae3b8807e0c18a29ab6a
Bhaumik Gadani <bhaumikwork@gmail.com>
2018-05-31 12:31:20 +0530

    chjanges

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
+  config.active_storage.service = :local
 
   
   # ActionMailer Config
