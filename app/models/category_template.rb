f63302206af55e7ff77624583b1e2961ce34e8fd
Nency <nency.tps@gmail.com>
2018-06-12 18:24:00 +0530

    delete template

--- app/models/category_template.rb
+++ app/models/category_template.rb
@@ -2,7 +2,7 @@ class CategoryTemplate < ApplicationRecord
   serialize [ :name ,:price,:description, :short_description, :image, :multiple_images, :google_map_link, :product_type, :map_location, :location, :mon_to_sat_on, :mon_to_sat_open_time, :mon_to_sat_close_time, :entry_fee_adult, :entry_fee_toddler, :entry_fee_child, :entry_fee_senior_citizen, :ratings,:parking_type, :parking_fees]
   store_accessor [:name,:price, :description, :short_description, :image, :multiple_images, :google_map_link, :product_type, :map_location, :location, :mon_to_sat_on, :mon_to_sat_open_time, :mon_to_sat_close_time, :entry_fee_adult, :entry_fee_toddler, :entry_fee_child, :entry_fee_senior_citizen, :ratings,:parking_type, :parking_fees],  :required, :optional
 
-  has_one :category
+  has_one :category, dependent: :nullify
   has_many :products, through: :category
 
   def get_template_fields
