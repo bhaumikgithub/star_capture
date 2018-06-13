593d18bc45dda93248c73a8b65cfdf9481107f84
Nency <nency.tps@gmail.com>
2018-06-13 16:38:41 +0530

    quick fix

--- app/controllers/category_templates_controller.rb
+++ app/controllers/category_templates_controller.rb
@@ -2,6 +2,10 @@ class CategoryTemplatesController < ApplicationController
 
   include InheritAction 
 
+  def index
+    @resources = CategoryTemplate.includes(:category).order('created_at DESC').page(params[:page]).per(10)
+  end
+
   private
 
   def resource_params
