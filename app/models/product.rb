d43af49f9e437b265ffa4c6baba51fdd4031f708
Nency <nency.tps@gmail.com>
2018-06-13 15:45:44 +0530

    query optimize

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   has_one_attached :image
   has_and_belongs_to_many :categories
   belongs_to :product_type
+  delegate :category_template, :to => :categories, :allow_nil => true
 
 
   # after_validation :remove_images
