dd50ebdd2ec9d19812d6e922a461d39e9f1c9aed
nencywadhvana <nencywadhvana@gmail.com>
2018-06-25 16:20:23 +0530

    change for no drop and no pickup

--- app/assets/javascripts/itineraries.js
+++ app/assets/javascripts/itineraries.js
@@ -1,15 +1,9 @@
 $( document ).ready(function() {
-
   pickup_not_fixed();
   drop_not_fixed();
-
-  $.each($("input[type=checkbox]:checked"), function(){            
-      $(this).closest('.row').find('.timings').show();
-  });
 });
 
 $(document).on('turbolinks:load', function() {
-
   pickup_not_fixed();
   drop_not_fixed();
 
@@ -18,11 +12,10 @@ $(document).on('turbolinks:load', function() {
 function pickup_not_fixed() {
   $(document).on('change', '.pickup_not_fixed', function () {
     if (this.checked) {
-      $(this).closest('.pickup_attributes').find('.pickup').attr('disabled', true)
+      $(this).closest('.pickup_attributes').find('.pickup').attr('disabled', true).val('');
     }
     else{
       $(this).closest('.pickup_attributes').find('.pickup').attr('disabled', false)
-      
     }
   })
 }
@@ -31,7 +24,7 @@ function pickup_not_fixed() {
 function drop_not_fixed() {
   $(document).on('change', '.drop_not_fixed', function () {
     if (this.checked) {
-      $(this).closest('.drop_attributes').find('.drop').attr('disabled', true)
+      $(this).closest('.drop_attributes').find('.drop').attr('disabled', true).val('');
     }
     else{
       $(this).closest('.drop_attributes').find('.drop').attr('disabled', false)
